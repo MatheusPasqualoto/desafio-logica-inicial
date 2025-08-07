148719b53987fa7be486fe275becd95fce8eb4d5
MatheusPasqualoto <matheusjpasqualoto@gmail.com>
2025-08-06 22:19:25 -0300

    commit teste

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-console.log("Hello World");
+let nome = "heroi";
+let xp = 3227;
+let nivel;
 
-let nome = "Matheus";
-console.log(nome);
+if(xp <= 1000){
+    nivel = "Ferro";
+}else if(xp >= 1001 && xp <=2000){
+    nivel = "Bronze";
+}else if(xp >= 2001 && xp <=5000){
+    nivel = "Prata";
+}else if(xp >= 5001 && xp <=7000){
+    nivel = "Ouro";
+}else if(xp >=7001 && xp <=8000){
+    nivel = "Platina";
+}else if(xp >= 8001 && xp <= 9000){
+    nivel = "Ascendente";
+}else if(xp >= 9001 && xp <= 10000){
+    nivel = "Imortal";
+}else{
+    nivel = "Radiante";
+}
+
+console.log("O Herói de nome ${nome} está no nível de ${nivel}");
+/*
+
+Ao final deve se exibir uma mensagem:
+"O Herói de nome ${nome} está no nível de **{nivel}**"
+ */
